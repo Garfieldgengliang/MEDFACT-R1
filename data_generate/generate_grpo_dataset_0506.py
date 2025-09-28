594eda1863396ab05fa81d64ec9013bb5962a7f8
ShuttleChaser <gengliang1997@gmail.com>
2025-09-28 14:50:57 +0800

    Commit Sep 28th 6

--- data_generate/generate_grpo_dataset_0506.py
+++ data_generate/generate_grpo_dataset_0506.py
@@ -2,36 +2,34 @@ from datasets import Dataset, Features, Image, Value
 
 import json
 
-with open('grpo_data_20250303.json', 'r') as file:
+with open('grpo_data_20250506.json', 'r') as file:
     grpo_dict = json.loads(file.read())
 image_path_list = grpo_dict['image_path']
-new_image_path_list = []
-for item in image_path_list:
-    new_image_path_list.append('/root/autodl-tmp/mimic_used/'+ item)
-    
+# new_image_path_list = []
+# for item in image_path_list:
+#     new_image_path_list.append('/root/autodl-tmp/IUxray/images/'+ item)
+# print(new_image_path_list)
 
 
 features = Features({
     "question_id": Value("int32"),
     "image": Image(),
     "question": Value("string"),
-    "answer": Value("string"),
-    "semantic_type":Value("string"),
-    "content_type":Value("string")
+    "solution": Value("string"),
+    "keyword": Value("string"),
     
 })
 
 dataset = Dataset.from_dict({
-    "question_id": list(range(len(new_image_path_list))),
-    "image": new_image_path_list,
+    "question_id": list(range(len(image_path_list))),
+    "image": image_path_list,
     "question": grpo_dict['question'],
-    "answer": grpo_dict['answer'],
-    "semantic_type":grpo_dict['semantic_type'],
-    "content_type":grpo_dict['content_type']
+    "solution": grpo_dict['solution'],
+    "keyword": grpo_dict['keyword'],
 }, features=features)
 
 split_dataset = dataset.train_test_split(test_size=0.1, seed=42)
-split_dataset.save_to_disk("/root/autodl-tmp/mimic_grpo_0306")
+split_dataset.save_to_disk("/root/autodl-tmp/IUxray_grpo_0506")
 # train_data = split_dataset["train"]
 # test_data = split_dataset["test"]
 
